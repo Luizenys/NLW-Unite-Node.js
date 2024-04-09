cf6f77ef790f681569874f2d8c4978c8b412c1f0
Luiza Rodrigues <luizars@id.uff.br>
2024-04-08 23:55:21 -0300

    feat: implementing error handler

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import { errorHandler } from "./error-handler";
 
 const app = fastify()
 
@@ -40,6 +41,7 @@ app.register(registerForEvent)
 app.register(checkIn)
 app.register(getEventAttendees)
 
+app.setErrorHandler(errorHandler)
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
